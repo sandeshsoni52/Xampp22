9553af9fcd53cdca2f178f38679e8a3cba5fbbd5
unknown <learnersolution2024@gmail.com>
2025-03-22 13:24:04 +0530

    connected DB okk

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-echo "Hello, World! from x22";
+$servername = "localhost";
+$username = "root"; // Default XAMPP user
+$password = ""; // No password by default
+$dbname = "test_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+$conn->close();
 ?>
